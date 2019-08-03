4bc93e3a7525206ee30e064fbf15f51e73d778ef
eempey <eempey@users.noreply.github.com>
2019-08-03 11:01:31 -0700

    Simple todo App

--- react/todo-app/src/App.js
+++ react/todo-app/src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            inputText: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({inputText: event.target.value})
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const data = [...this.state.data, this.state.inputText];
+        this.setState({data, inputText: ''});
+    }
+
+    render() {
+        const itemList = this.state.data.map((item, index) => (
+                <li key={index}>{item}</li>
+            ));
+
+        return (
+            <div className="App">
+                <h1>Simple Todo App</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        name="inputText"
+                        value={this.state.inputText}
+                        onChange={this.handleChange}
+                    />
+                    <button type="submit">
+                        Add an item
+                    </button>
+                </form>
+                <ol>
+                    {itemList}
+                </ol>
+            </div>
+        )
+    }
 }
 
 export default App;
