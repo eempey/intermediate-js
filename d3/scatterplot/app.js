75cdd461abc2f881d9a50f18be75701635c60700
eempey <eempey@users.noreply.github.com>
2019-12-28 00:51:44 -0800

    Adding axes and labels to scatterplot

--- d3/scatterplot/app.js
+++ d3/scatterplot/app.js
@@ -1,6 +1,6 @@
 const width = 500;
 const height = 500;
-const padding = 20;
+const padding = 30;
 
 const yScale = d3.scaleLinear()
                 .domain(d3.extent(birthData2011, d => d.lifeExpectancy))
@@ -10,6 +10,13 @@ const xScale = d3.scaleLinear()
                 .domain(d3.extent(birthData2011, d => d.births / d.population))
                 .range([padding, width - padding]);
 
+const xAxis = d3.axisBottom(xScale)
+                .tickSize(-height + 2 * padding)
+                .tickSizeOuter(0);
+const yAxis = d3.axisLeft(yScale)
+                .tickSize(-width + 2 * padding)
+                .tickSizeOuter(0);
+
 const colorScale = d3.scaleLinear()
                     .domain(d3.extent(birthData2011, d => d.population/d.area))
                     .range(['lightgreen', 'black']);
@@ -18,6 +25,16 @@ const radiusScale = d3.scaleLinear()
                         .domain(d3.extent(birthData2011, d => d.births))
                         .range([2, 40]);
 
+
+d3.select('svg')
+    .append('g')
+    .attr('transform', 'translate(0,' + (height - padding) + ')')
+    .call(xAxis);
+
+d3.select('svg')
+    .append('g')
+    .attr('transform', 'translate(' + padding + ',0)')
+    .call(yAxis);
 
 d3.select('svg')
         .attr('width', width)
@@ -30,3 +47,28 @@ d3.select('svg')
         .attr('cy', d => yScale(d.lifeExpectancy))
         .attr('r', d => radiusScale(d.births))
         .attr('fill', d => colorScale(d.population / d.area));
+
+d3.select('svg')
+    .append('text')
+        .attr('x', width / 2)
+        .attr('y', height-padding)
+        .attr('dy', '1.5em')
+        .style('text-anchor', 'middle')
+        .text('Births per Capita');
+
+d3.select('svg')
+    .append('text')
+    .attr('x', width / 2)
+    .attr('y', padding)
+    .style('font-size', '1.5em')
+    .style('text-anchor', 'middle')
+    .text('Data on Births by Country in 2011');
+
+d3.select('svg')
+.append('text')
+.attr('transform', 'rotate(-90)')
+.attr('x', -height / 2)
+.attr('y', padding)
+.attr('dy', '-1.1em')
+.style('text-anchor', 'middle')
+.text('Life Expectancy');
