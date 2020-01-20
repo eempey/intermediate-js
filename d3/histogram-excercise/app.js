d2e5e69d48a51cb3f68bd3a04f55b56f16338ec6
eempey <eempey@users.noreply.github.com>
2020-01-19 22:05:13 -0800

    Histogram exercise

---
+++ d3/histogram-excercise/app.js
@@ -0,0 +1,54 @@
+const width = 600;
+const height = 600;
+const padding = 20;
+const barPadding = 1;
+const minValue = d3.min(regionData, d => d.adultLiteracyRate);
+const maxValue = d3.max(regionData, d => d.adultLiteracyRate);
+const adultLiteracyRateData = regionData.filter(d => d.adultLiteracyRate !== null);
+
+const xScale = d3.scaleLinear()
+    .domain([0, d3.max(adultLiteracyRateData, d => d.adultLiteracyRate)])
+    .rangeRound([padding, width - padding]);
+
+const histogram = d3.histogram()
+    .domain(xScale.domain())
+    .thresholds(xScale.ticks())
+    .value(d => d.adultLiteracyRate);
+
+const bins = histogram(adultLiteracyRateData);
+
+const yScale = d3.scaleLinear()
+    .domain([0, d3.max(bins, d => d.length)])
+    .range([height, 0]);
+
+const xAxis = d3.axisBottom(xScale)
+    .tickSize(5)
+    .tickSizeOuter(27);
+
+d3.select('svg')
+    .append('g')
+    .attr('transform', `translate(0, ${height - padding})`)
+    .call(xAxis);
+
+const bars = d3.select('svg')
+        .attr('width', width)
+        .attr('height', height)
+    .selectAll('.bar')
+    .data(bins)
+    .enter()
+    .append('g')
+        .classed('bar', true);
+
+bars.append('rect')
+    .attr('x', d => xScale(d.x0))
+    .attr('y', d => yScale(d.length))
+    .attr('height', d => height - yScale(d.length))
+    .attr('width', d => {
+        const width = xScale(d.x1) - xScale(d.x0) - barPadding;
+        return width > 0 ? width : 0;
+    })
+    .attr('fill', '#9c27b0');
+
+//
+
+
